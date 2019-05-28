ed66f4438b060005fd3555106faf78a5ab50b844
Michelle Moon <michellemoon236@gmail.com>
2019-05-28 22:42:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/' do
-    
+    "Hello, World!"
   end
   
   get '/name' do
-    
+    "My name is __"
   end
   
   get '/hometown' do
